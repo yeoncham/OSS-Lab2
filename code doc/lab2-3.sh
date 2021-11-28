9641dd9c2c168f1afd2be7d73b98ed51e0f9a309
yeoncham <90040072+yeoncham@users.noreply.github.com>
2021-11-28 12:09:39 +0900

    Create lab2-3.sh

---
+++ code doc/lab2-3.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+echo "-----------------------------------"
+echo "BMI 지수 계산하기"
+echo "-----------------------------------"
+read -p "몸무게를 kg단위로 입력하세요: " weight
+read -p "키를 m단위로 입력하세요: " height
+bmi=`expr "scale=2; $weight / ( $height * $height )" |bc`
+echo $bmi
+if [ 1 -eq "$(( echo "$bmi < 18.5") | bc )" ]
+then
+echo "저체중입니다"
+elif [ 1 -eq "$(( echo "$bmi < 23") | bc )" ]
+then
+echo "정상입니다"
+else
+echo "과체중입니다"
+fi
+exit 0
